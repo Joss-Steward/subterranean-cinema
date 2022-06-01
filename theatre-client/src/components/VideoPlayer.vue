962da6e838421da847d70de49af1daaf04fe2e5e
Joss Steward <joss@42hz.net>
2022-05-31 19:05:12 -0700

    vue.js front end

---
+++ theatre-client/src/components/VideoPlayer.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <video ref="videoPlayer" class="video-js"></video>
+    </div>
+</template>
+
+<script>
+import videojs from 'video.js';
+import 'video.js/dist/video-js.min.css';
+
+export default {
+    name: 'VideoPlayer',
+    props: {
+        options: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    },
+    data() {
+        return {
+            player: null
+        }
+    },
+    mounted() {
+        this.player = videojs(this.$refs.videoPlayer, this.options, () => {
+            this.player.log('onPlayerReady', this);
+        });
+        console.log(this.player);
+    },
+    beforeDestroy() {
+        if (this.player) {
+            this.player.dispose();
+        }
+    }
+}
+</script>
---
+++ theatre-client/src/views/VideoView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <video-player :options="videoOptions" />
+    </div>
+</template>
+
+<script>
+import VideoPlayer from '@/components/VideoPlayer.vue';
+
+export default {
+    name: 'VideoExample',
+    components: {
+        VideoPlayer
+    },
+    data() {
+        return {
+            videoOptions: {
+                autoplay: true,
+                controls: true,
+                sources: [
+                    {
+                        src:  'http://localhost:8080/media/ballard.mp4',
+                        type: 'video/mp4'
+                    }
+                ]
+            }
+        };
+    }
+};
+</script>
